847a1eeecbb95a9b8253ae7afa139ef33f27783c
Kirill Zaborsky <avto.oil@gmail.com>
2021-11-30 13:30:23 +0700

    Initial Commit

---
+++ Berg Client/Berg_ClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  Berg_ClientApp.swift
+//  Berg Client
+//
+//  Created by Кирилл Заборский on 30.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct Berg_ClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Berg Client/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Berg Client
+//
+//  Created by Кирилл Заборский on 30.11.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
